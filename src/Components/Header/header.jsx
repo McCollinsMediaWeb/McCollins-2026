18fa56f4daf6d348cd93651c2135674ab67c5eb9
McCollinsMediaWeb <111267011+McCollinsMediaWeb@users.noreply.github.com>
2026-03-02 11:51:42 +0530

    555

--- src/Components/Header/header.jsx
+++ src/Components/Header/header.jsx
@@ -5,32 +5,13 @@ import ThemeSwitcher from "../Theme/themeswitch";
 import { useNav } from "../Context/NavContext";
 
 function Navbar() {
-    // useEffect(() => {
-    //     const handleScroll = () => {
-    //         const scrollTop = window.scrollY;
-    //         const viewportHeight = window.innerHeight;
-    //         const fullHeight = document.documentElement.scrollHeight;
-
-    //         if (scrollTop + viewportHeight >= fullHeight - 5) {
-    //             document.body.classList.add("ScrollInitiated");
-    //         } else {
-    //             document.body.classList.remove("ScrollInitiated");
-    //         }
-    //     };
-
-    //     window.addEventListener("scroll", handleScroll);
-
-    //     return () => {
-    //         window.removeEventListener("scroll", handleScroll);
-    //     };
-    // }, []);
-
-
     useEffect(() => {
         const handleScroll = () => {
             const scrollTop = window.scrollY;
+            const viewportHeight = window.innerHeight;
+            const fullHeight = document.documentElement.scrollHeight;
 
-            if (scrollTop > 80) {   // <-- trigger point
+            if (scrollTop + viewportHeight >= fullHeight - 5) {
                 document.body.classList.add("ScrollInitiated");
             } else {
                 document.body.classList.remove("ScrollInitiated");
@@ -39,48 +20,71 @@ function Navbar() {
 
         window.addEventListener("scroll", handleScroll);
 
-        // run once on mount (IMPORTANT)
-        handleScroll();
-
         return () => {
             window.removeEventListener("scroll", handleScroll);
         };
     }, []);
 
-
-    const { isDropdownActive } = useNav();
-
     return (
-        <div className="FixedHeader">
-            <div className="container">
-                <div className="HeaderBox">
-                    <div className="HeaderBoxFlex">
-                        <div className="HeaderBoxLeft">
-                            <div className="HamberMenuIcon">
-                                <div className="HamberMenuLine"></div>
-                                <div className="HamberMenuLine"></div>
-                                <div className="HamberMenuLine"></div>
-                            </div>
+        <>
+            <div className="FixedHeader">
+                <div className="container">
+                    <div className="HeaderBox">
+                        <div className="HeaderBoxFlex">
+                            <div className="HeaderBoxLeft">
+                                <div className="HamberMenuIcon">
+                                    <div className="HamberMenuLine"></div>
+                                    <div className="HamberMenuLine"></div>
+                                    <div className="HamberMenuLine"></div>
+                                </div>
 
-                        </div>
-                        <div className="HeaderBoxMiddle">
-                            <NavLink className="navbar-brand" to="/">
-                                <img
-                                    src="/assets/images/mcmain.png"
-                                    className="site-logo img-fluid"
-                                    alt="Logo"
-                                />
-                            </NavLink>
-                        </div>
-                        <div className="HeaderBoxRight">
-                            <a href="#" className="CallLink btn btn-accent">
-                                Let's Talk
-                            </a>
+                            </div>
+                            <div className="HeaderBoxMiddle">
+                                <NavLink className="navbar-brand" to="/">
+                                    <img
+                                        src="/assets/images/mcmain.png"
+                                        className="site-logo img-fluid"
+                                        alt="Logo"
+                                    />
+                                </NavLink>
+                            </div>
+                            <div className="HeaderBoxRight">
+                                <a href="#" className="CallLink btn btn-accent">
+                                    Let's Talk
+                                </a>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+            <div className="MegaMenu">
+                <div className="MegamenuHeader">
+                    <div className="container">
+                        <div className="HdMenuFlex">
+                            <div className="HdMenuLeft">
+                                <NavLink className="navbar-brand" to="/">
+                                    <img
+                                        src="/assets/images/mcmain.png"
+                                        className="site-logo img-fluid"
+                                        alt="Logo"
+                                    />
+                                </NavLink>
+                            </div>
+                            <div className="HdMenuMiddle">
+                                <div className="HdT1">Explore Menu</div>
+                            </div>
+                            <div className="HdMenuRight">
+                                <div className="CloseBtn">&nbsp;</div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <div className="MegaMenuMiddle">
+                    <div className="container">Ijas</div>
+                </div>
+            </div>
+        </>
     );
 }
 
