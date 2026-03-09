b7c183a278f483b3a2801df5cebd6ca763265f52
McCollinsMediaWeb <111267011+McCollinsMediaWeb@users.noreply.github.com>
2026-03-09 14:32:28 +0530

    888

--- src/Page/About/index.jsx
+++ src/Page/About/index.jsx
@@ -10,13 +10,93 @@ import ModalVideoSection from "../../Components/Video/video";
 import DigitalProcessSection from "../../Components/DigitalProcess/digitalstep";
 import TestimonialSection from "../../Components/Testimonial/testimonial";
 import InnerNew from "../../Components/Banner/InnerNew";
+import AnimateOnScroll from "../../Components/Hooks/AnimateOnScroll";
 
-function AboutPage(){
-    return(
+function AboutPage() {
+    return (
         <>
-            <HeadTitle title="About - McCollins Media"/>
-            {/* <BannerInnerSection title="About Marko" currentPage="About Us" /> */}
+            <HeadTitle title="About - McCollins Media" />
+
             <InnerNew desktopImage="/assets/images/abtdesk.jpg" mobileImage="/assets/images/abtmob.jpg" title="Strategically Rooted." title2="Creatively Fueled." title3="Insight Driven." />
+            <AnimateOnScroll animation="fadeInUp">
+                <div>
+                    <div className="section HdLayt1">
+                        <div className="container">
+                            <div className="expertise-title">
+                                <AnimateOnScroll animation="fadeInRight" speed="normal">
+                                    <div className="sub-heading">
+                                        <i className="fa-regular fa-circle-dot"></i>
+                                        <span>About Us</span>
+                                    </div>
+                                </AnimateOnScroll>
+
+                                <AnimateOnScroll animation="fadeInRight" speed="normal">
+                                    <div className="row">
+                                        <div className="col-md-7">
+                                            <h2 className="title-heading">
+                                                Built on Strategy<br /> Defined by Insight
+                                            </h2>
+                                        </div>
+                                        <div className="col-md-5">
+                                            <div className="HdT1">A Voice For Your Brand</div>
+                                            <p>
+                                                Partnering With Ambitious Brands To Transform Cultural Intelligence Into Market-Leading Digital Influence.
+                                            </p>
+                                        </div>
+                                    </div>
+                                </AnimateOnScroll>
+                                <div className="AbtParaBox">
+                                    <AnimateOnScroll animation="fadeInRight" speed="normal">
+
+                                        <p>McCollins Media is a Digital Brand Activation agency specializing in Mobile-first technology and Performance-Driven Marketing campaigns in the MENA region. We are Headquartered in Dubai with a complete in-house team of creative professionals.</p>
+
+                                    </AnimateOnScroll>
+                                    <AnimateOnScroll animation="fadeInRight" speed="normal">
+                                        <p>We are in the pursuit of providing digital advancement to all our clients to take their brand to the next level. Be it Web Development, Google Ads, 360 Brand Development, or Digital Marketing, we do everything in-house.
+                                        </p>
+                                    </AnimateOnScroll>
+                                    <AnimateOnScroll animation="fadeInRight" speed="normal">
+
+                                        <p>Our multi-cultural team is always daring to set viral trends, venture into the digital realm, and take ownership of the brands we represent. After 15 years of being a pioneer Digital Marketing agency in the UAE, we’re just starting what we are here for!</p>
+                                    </AnimateOnScroll>
+                                </div>
+
+                            </div>
+                            <div className="section BoxItem1">
+                                <h2 className="title-heading text-center">
+                                    Where Strategic Precision Meets<br /> Global Creative Excellence.
+                                </h2>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </AnimateOnScroll>
+
+            <div className="AbtRows">
+                <AnimateOnScroll animation="fadeInUp">
+                    <div className="AbtRow">
+                        <div className="container">
+                            <div className="row align-items-center">
+                                <div className="col-md-8">
+                                    <img className="width-full abtImage" src="/assets/images/abt1.jpg" />
+                                </div>
+                                <div className="col-md-4">
+                                    <div className="AbtRowContent">
+                                        <div className="AbtT1">Mastering Ecosystems. Delivering Results.</div>
+                                        <div className="AbtT2">Managing the digital ecosystem for your
+brand with a result</div>
+                                        <div className="AbtT3">From Digital Media Buying, Campaign Development, Blogger Outreach Programs to Content Development, we do it all in-house.</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </AnimateOnScroll>
+            </div>
+
+
+
             {/* <AboutSection />
             <PartnershipSection />
             <ChooseUsAboutSection />
