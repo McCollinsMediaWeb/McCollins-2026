648b6d12e928a2d92bb961eae7d9e6e22eef617d
McCollinsMediaWeb <111267011+McCollinsMediaWeb@users.noreply.github.com>
2026-06-04 14:47:08 +0530

    55

--- src/Components/Banner/FullSizeBanner.jsx
+++ src/Components/Banner/FullSizeBanner.jsx
@@ -4,7 +4,20 @@ import AnimateOnScroll from "../Hooks/AnimateOnScroll";
 function FullSizeBanner({ desktopImage, mobileImage, altText = "Services Banner" }) {
     return (
         <div className="fullsize-banner-wrapper" style={{ width: "100%", overflow: "hidden" }}>
-            <AnimateOnScroll animation="fadeInUp" speed="normal">
+            <style>
+                {`
+                    .fullsize-banner-wrapper {
+                        cursor: pointer;
+                    }
+                    .fullsize-banner-wrapper img {
+                        transition: transform 1.5s cubic-bezier(0.25, 1, 0.5, 1) !important;
+                    }
+                    .fullsize-banner-wrapper:hover img {
+                        transform: scale(1.04);
+                    }
+                `}
+            </style>
+            <AnimateOnScroll animation="fadeInUp" speed="slow">
                 <picture style={{ width: "100%", display: "block" }}>
                     {/* Mobile View Image */}
                     <source media="(max-width: 768px)" srcSet={mobileImage} />
