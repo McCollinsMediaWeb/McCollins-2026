5cbe695a6578da87a7dd61dad576d41c3dfbf16e
McCollinsMediaWeb <ijas@mccollinsmedia.com>
2026-06-15 16:25:41 +0530

    feat: add full size showcase banners to Social Media Marketing landing page

--- src/Page/SocialMediaMarketing/index.jsx
+++ src/Page/SocialMediaMarketing/index.jsx
@@ -7,6 +7,7 @@ import ClientPortfolio from "../../Components/MccollinsMedia/ClientPortfolio";
 import SocialMediaMarketingImageWithText from "../../Components/MccollinsMedia/SocialMediaMarketing";
 import CaseStudiesSocialMediaMarketing from "../../Components/CaseStudies/CaseStudiesSocialMediaMarketing";
 import OurPartners from "../../Components/MccollinsMedia/OurPartners";
+import FullSizeBanner from "../../Components/Banner/FullSizeBanner";
 
 function SocialMediaMarketing() {
     return (
@@ -17,6 +18,53 @@ function SocialMediaMarketing() {
             <MarketingFoundation />
             <ClientPortfolio />
             <SocialMediaMarketingImageWithText />
+            
+            <FullSizeBanner 
+                desktopImage="/services/social-media-marketing/McC_SocMedMark_Web_1-min.jpg" 
+                mobileImage="/services/social-media-marketing/McC_SocMedMark_Mob_1-min.jpg" 
+                altText="Social Media Marketing Showcase 1" 
+            />
+            <FullSizeBanner 
+                desktopImage="/services/social-media-marketing/McC_SocMedMark_Web_2-min.jpg" 
+                mobileImage="/services/social-media-marketing/McC_SocMedMark_Mob_2-min.jpg" 
+                altText="Social Media Marketing Showcase 2" 
+            />
+            <FullSizeBanner 
+                desktopImage="/services/social-media-marketing/McC_SocMedMark_Web_3-min.jpg" 
+                mobileImage="/services/social-media-marketing/McC_SocMedMark_Mob_3-min.jpg" 
+                altText="Social Media Marketing Showcase 3" 
+            />
+            <FullSizeBanner 
+                desktopImage="/services/social-media-marketing/McC_SocMedMark_Web_4-min.jpg" 
+                mobileImage="/services/social-media-marketing/McC_SocMedMark_Mob_4-min.jpg" 
+                altText="Social Media Marketing Showcase 4" 
+            />
+            <FullSizeBanner 
+                desktopImage="/services/social-media-marketing/McC_SocMedMark_Web_6-min.jpg" 
+                mobileImage="/services/social-media-marketing/McC_SocMedMark_Mob_6-min.jpg" 
+                altText="Social Media Marketing Showcase 5" 
+            />
+            <FullSizeBanner 
+                desktopImage="/services/social-media-marketing/McC_SocMedMark_Web_7-min.jpg" 
+                mobileImage="/services/social-media-marketing/McC_SocMedMark_Mob_7-min.jpg" 
+                altText="Social Media Marketing Showcase 6" 
+            />
+            <FullSizeBanner 
+                desktopImage="/services/social-media-marketing/McC_SocMedMark_Web_9-min.jpg" 
+                mobileImage="/services/social-media-marketing/McC_SocMedMark_Mob_9-min.jpg" 
+                altText="Social Media Marketing Showcase 7" 
+            />
+            <FullSizeBanner 
+                desktopImage="/services/social-media-marketing/McC_SocMedMark_Web_10-min.jpg" 
+                mobileImage="/services/social-media-marketing/McC_SocMedMark_Mob_10-min.jpg" 
+                altText="Social Media Marketing Showcase 8" 
+            />
+            <FullSizeBanner 
+                desktopImage="/services/social-media-marketing/McC_SocMedMark_Web_11-min.jpg" 
+                mobileImage="/services/social-media-marketing/McC_SocMedMark_Mob_11-min.jpg" 
+                altText="Social Media Marketing Showcase 9" 
+            />
+
             <div className="container mb-5">
                 <div style={{ height: '1px', backgroundColor: '#d6d6d6', width: "100%" }} ></div>
             </div>
